d65dff73921484ea1d2a67bacc2bbcfd05343c83
nalakagunathilaka <nalakagunathilaka@gmail.com>
2021-05-11 14:10:40 +0530

    Add common and toolkits modules

---
+++ components/com.wso2.openbanking.cds.common/src/main/java/com/wso2/openbanking/cds/common/utils/CommonConstants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses. For specific
+ * language governing the permissions and limitations under this license,
+ * please see the license as well as any agreement you’ve entered into with
+ * WSO2 governing the purchase of this software and any associated services.
+ */
+
+package com.wso2.openbanking.cds.common.utils;
+
+/**
+ * Common Constant Class
+ */
+public class CommonConstants {
+
+    public static final String OB_CONFIG_FILE = "open-banking-cds.xml";
+    public static final String OB_CDS_CONFIG_QNAME = "http://wso2.org/projects/carbon/open-banking-cds.xml";
+    public static final String DCR_CONFIG_TAG = "DCR";
+    public static final String CONSENT_MGT_CONFIG_TAG = "ConsentManagement";
+    public static final String METHOD_CONFIG_TAG = "Method";
+}
