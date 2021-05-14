8a01946d7b935a3cbb0599e98c150221b5a608da
nalakagunathilaka <nalakagunathilaka@gmail.com>
2021-05-14 14:43:41 +0530

    Add WSO2 software license header

--- toolkits/ob-is/bin/merge.sh
+++ toolkits/ob-is/bin/merge.sh
@@ -6,14 +6,14 @@
 # This software is the property of WSO2 Inc. and its suppliers, if any.
 # Dissemination of any information or reproduction of any material contained
 # herein is strictly forbidden, unless permitted by WSO2 in accordance with
-# the WSO2 Commercial License available at http://wso2.com/licenses. For specific
+# the WSO2 Software License available at https://wso2.com/licenses/eula/3.1. For specific
 # language governing the permissions and limitations under this license,
 # please see the license as well as any agreement you’ve entered into with
 # WSO2 governing the purchase of this software and any associated services.
 #
 # ------------------------------------------------------------------------
 
-# merge.sh script copy the WSO2 OB IS UK Toolkit artifacts on top of WSO2 IS base product
+# merge.sh script copy the WSO2 OB IS CDS Toolkit artifacts on top of WSO2 IS base product
 #
 # merge.sh <WSO2_OB_IS_HOME>
 
