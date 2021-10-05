d2b74f16439bb8cf702cbefaec6cf3444384b084
vimukthir <vimukthir@wso2.com>
2021-10-06 01:08:05 +0530

    contains cds internal apis module metadata cache changes

---
+++ internal-apis/internal-webapps/com.wso2.openbanking.cds.metadata.mgt.endpoint/src/main/java/com/wso2/openbanking/cds/metadata/mgt/endpoint/api/MetadataMgtApi.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under this
+ * license, please see the license as well as any agreement you’ve entered into
+ * with WSO2 governing the purchase of this software and any associated services.
+ *
+ */
+
+package com.wso2.openbanking.cds.metadata.mgt.endpoint.api;
+
+import com.wso2.openbanking.cds.metadata.mgt.endpoint.model.MetadataUpdateRequestDTO;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import javax.ws.rs.core.Response;
+
+/**
+ *  Metadata Management API
+ */
+@Path("/admin")
+public interface MetadataMgtApi {
+
+    /**
+     * Metadata Update.
+     * Indicate that a critical update to the metadata for Accredited Data Recipients has been made and
+     * should be obtained.
+     */
+    @POST
+    @Path("/register/metadata")
+    @Consumes({"application/json"})
+    @ApiOperation(value = "Metadata Update", tags = {"Admin", "Register"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success")})
+    Response updateMetaData(@HeaderParam("x-v") @NotNull String xV, @Valid MetadataUpdateRequestDTO action,
+                                   @HeaderParam("x-min-v") String xMinV);
+
+}
---
+++ internal-apis/internal-webapps/com.wso2.openbanking.cds.metadata.mgt.endpoint/src/main/java/com/wso2/openbanking/cds/metadata/mgt/endpoint/model/MetadataUpdateRequestDTO.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under this
+ * license, please see the license as well as any agreement you’ve entered into
+ * with WSO2 governing the purchase of this software and any associated services.
+ *
+ */
+
+package com.wso2.openbanking.cds.metadata.mgt.endpoint.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+/**
+ *  MetaData Update Request DTO
+ */
+public class MetadataUpdateRequestDTO {
+    @ApiModelProperty(required = true, value = "")
+    @Valid
+    private MetadataUpdateDataDTO data;
+
+    @ApiModelProperty(value = "")
+    @Valid
+    private Object meta;
+
+    /**
+     * Get data.
+     *
+     * @return data
+     **/
+    @JsonProperty("data")
+    @NotNull
+    public MetadataUpdateDataDTO getData() {
+        return data;
+    }
+
+    public void setData(MetadataUpdateDataDTO data) {
+        this.data = data;
+    }
+
+    public MetadataUpdateRequestDTO data(MetadataUpdateDataDTO data) {
+        this.data = data;
+        return this;
+    }
+
+    /**
+     * Get meta.
+     *
+     * @return meta
+     **/
+    @JsonProperty("meta")
+    public Object getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Object meta) {
+        this.meta = meta;
+    }
+
+    public MetadataUpdateRequestDTO meta(Object meta) {
+        this.meta = meta;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class RequestMetaDataUpdateDTO {\n");
+
+        sb.append("    data: ").append(toIndentedString(data)).append("\n");
+        sb.append("    meta: ").append(toIndentedString(meta)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private static String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
