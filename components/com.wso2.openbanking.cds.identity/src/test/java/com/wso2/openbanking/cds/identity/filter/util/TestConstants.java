a60302dcf9ff7650f0db30b62383839dc5670199
imesh94 <imeshu@wso2.com>
2021-12-10 14:23:34 +0530

    Update licence headers

--- components/com.wso2.openbanking.cds.identity/src/test/java/com/wso2/openbanking/cds/identity/filter/util/TestConstants.java
+++ components/com.wso2.openbanking.cds.identity/src/test/java/com/wso2/openbanking/cds/identity/filter/util/TestConstants.java
@@ -4,12 +4,11 @@
  * This software is the property of WSO2 Inc. and its suppliers, if any.
  * Dissemination of any information or reproduction of any material contained
  * herein is strictly forbidden, unless permitted by WSO2 in accordance with
- * the WSO2 Commercial License available at http://wso2.com/licenses. For specific
+ * the WSO2 Software License available at https://wso2.com/licenses/eula/3.1. For specific
  * language governing the permissions and limitations under this license,
  * please see the license as well as any agreement you’ve entered into with
  * WSO2 governing the purchase of this software and any associated services.
  */
-
 package com.wso2.openbanking.cds.identity.filter.util;
 
 public class TestConstants {
