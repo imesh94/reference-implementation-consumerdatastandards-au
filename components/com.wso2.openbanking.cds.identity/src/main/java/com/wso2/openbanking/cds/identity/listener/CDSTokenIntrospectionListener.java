81e4f7b210fe09626efebe0261e79a9cc18b2df2
DedunuKarunarathne <46235093+DedunuKarunarathne@users.noreply.github.com>
2023-06-15 08:51:27 +0530

    Fix PR comments

--- components/com.wso2.openbanking.cds.identity/src/main/java/com/wso2/openbanking/cds/identity/listener/CDSTokenIntrospectionListener.java
+++ components/com.wso2.openbanking.cds.identity/src/main/java/com/wso2/openbanking/cds/identity/listener/CDSTokenIntrospectionListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * This software is the property of WSO2 LLC. and its suppliers, if any.
  * Dissemination of any information or reproduction of any material contained
@@ -15,10 +15,11 @@ import org.wso2.carbon.identity.oauth2.dto.OAuth2TokenValidationRequestDTO;
 import java.util.Map;
 
 /**
- * Event listener for token instrospection.
+ * Event listener for token introspection.
  */
 public class CDSTokenIntrospectionListener extends AbstractOAuthEventInterceptor {
     private static final String REFRESH = "Refresh";
+
     /**
      * Allow token introspection for only the refresh token.
      *
@@ -35,7 +36,7 @@ public class CDSTokenIntrospectionListener extends AbstractOAuthEventInterceptor
         if (oAuth2IntrospectionResponseDTO.isActive() &&
                 !REFRESH.equalsIgnoreCase(oAuth2IntrospectionResponseDTO.getTokenType())) {
             // CDS specified only to support refresh token introspection.
-            // As in rfc7662 section-2.2 : returning as inactive, for the tokens that are not allowed to introspect,
+            // As in rfc7662 section-2.2 : returning as inactive, for the tokens that are not allowed to introspect.
             oAuth2IntrospectionResponseDTO.setActive(false);
             oAuth2IntrospectionResponseDTO.setError("Introspection is supported only for refresh tokens.");
             return;
