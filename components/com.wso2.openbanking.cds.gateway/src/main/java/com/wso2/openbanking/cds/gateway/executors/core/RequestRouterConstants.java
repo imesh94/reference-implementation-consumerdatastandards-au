7bea9b752927a3e52685b52f43e50c6f86361d8b
AmilaSamith <amilasamit@gmail.com>
2023-07-08 13:43:28 +0530

    add changes to request router for CDS_COMMON_API

--- components/com.wso2.openbanking.cds.gateway/src/main/java/com/wso2/openbanking/cds/gateway/executors/core/RequestRouterConstants.java
+++ components/com.wso2.openbanking.cds.gateway/src/main/java/com/wso2/openbanking/cds/gateway/executors/core/RequestRouterConstants.java
@@ -20,6 +20,7 @@ public class RequestRouterConstants {
     public static final String DEFAULT = "Default";
     public static final String DCR = "DCR";
     public static final String CDS = "CDS";
+    public static final String CDS_COMMON = "CDSCommon";
     public static final String CONSENT = "Consent";
     public static final String ADMIN = "Admin";
     public static final String ARRANGEMENT = "Arrangement";
@@ -30,12 +31,14 @@ public class RequestRouterConstants {
     public static final String API_TYPE_NON_REGULATORY = "non-regulatory";
     public static final String API_TYPE_DCR = "dcr";
     public static final String API_TYPE_CDS = "cds";
+    public static final String API_TYPE_COMMON = "common";
     public static final String API_TYPE_ARRANGEMENT = "arrangement";
     public static final String API_TYPE_ADMIN = "admin";
 
     // API Name constants
     public static final String DCR_API_NAME = "CDR Dynamic Client Registration API";
     public static final String CDS_API_NAME = "ConsumerDataStandards";
+    public static final String COMMON_API_NAME = "ConsumerDataStandardsCommon";
     public static final String ADMIN_API_NAME = "ConsumerDataStandardsAdminAPI";
     public static final String ARRANGEMENT_API_NAME = "CDR Arrangement Management API";
 }
