661fbd188178526a2e0457df9c2fc280f4e98eb8
Akram Azarm <akram.arm.7@gmail.com>
2023-07-12 21:17:06 +0530

    resolved PR comments

--- components/com.wso2.openbanking.cds.identity/src/main/java/com/wso2/openbanking/cds/identity/auth/extensions/response/validator/CDSCodeResponseTypeValidator.java
+++ components/com.wso2.openbanking.cds.identity/src/main/java/com/wso2/openbanking/cds/identity/auth/extensions/response/validator/CDSCodeResponseTypeValidator.java
@@ -12,6 +12,8 @@ package com.wso2.openbanking.cds.identity.auth.extensions.response.validator;
 import com.wso2.openbanking.accelerator.identity.util.IdentityCommonConstants;
 import com.wso2.openbanking.accelerator.identity.util.IdentityCommonUtil;
 import com.wso2.openbanking.cds.identity.utils.CDSIdentityConstants;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.oltu.oauth2.as.validator.TokenValidator;
 import org.apache.oltu.oauth2.common.OAuth;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
@@ -25,6 +27,8 @@ import javax.servlet.http.HttpServletRequest;
  * Validator for code flow token requests.
  */
 public class CDSCodeResponseTypeValidator extends TokenValidator {
+
+    private static final Log log = LogFactory.getLog(CDSCodeResponseTypeValidator.class);
 
     @Override
     public void validateRequiredParameters(HttpServletRequest request) throws OAuthProblemException {
@@ -42,6 +46,7 @@ public class CDSCodeResponseTypeValidator extends TokenValidator {
         //If the response type is "code", only the "jwt" response mode can be used.
         if (CDSIdentityConstants.CODE_RESPONSE_TYPE.equalsIgnoreCase(responseType) &&
                 !CDSIdentityConstants.JWT_RESPONSE_MODE.equalsIgnoreCase(responseMode)) {
+            log.error(CDSIdentityConstants.UNSUPPORTED_RESPONSE_MODE);
             throw OAuthProblemException.error(CDSIdentityConstants.UNSUPPORTED_RESPONSE_TYPE_ERROR)
                     .description("Unsupported Response Mode")
                     .state(state);
