fc57edb2aad131586f7b7650dfd0e61318fb4690
AmilaSamith <amilasamit@gmail.com>
2023-07-12 17:39:49 +0530

    update inline comments

--- components/com.wso2.openbanking.cds.consent.extensions/src/main/java/com/wso2/openbanking/cds/consent/extensions/authorize/utils/CDSConsentPersistUtil.java
+++ components/com.wso2.openbanking.cds.consent.extensions/src/main/java/com/wso2/openbanking/cds/consent/extensions/authorize/utils/CDSConsentPersistUtil.java
@@ -74,12 +74,15 @@ public class CDSConsentPersistUtil {
         // update Non-primary account against users map
         currentNonPrimaryAccountIdUsersMap.putAll(nonPrimaryAccountIdUsersMap);
 
-        // update user against account map
+        // Update user against accounts map
+        // Get UserId against non-primary accounts Map from current persistence Step
         Map<String, List<String>> userIdNonPrimaryAccountsMap = getUserIdAgainstAccountsMap(
                 nonPrimaryAccountIdUsersMap);
+        // Append current step user against accounts map details to the existing map
         for (Map.Entry<String, List<String>> entry : userIdNonPrimaryAccountsMap.entrySet()) {
             final String userId = entry.getKey();
             final List<String> nonPrimaryAccounts = entry.getValue();
+            // Check if the user already has an entry in the map
             if (currentUserIdNonPrimaryAccountsMap.containsKey(userId)) {
                 currentUserIdNonPrimaryAccountsMap.get(userId).addAll(nonPrimaryAccounts);
             } else {
