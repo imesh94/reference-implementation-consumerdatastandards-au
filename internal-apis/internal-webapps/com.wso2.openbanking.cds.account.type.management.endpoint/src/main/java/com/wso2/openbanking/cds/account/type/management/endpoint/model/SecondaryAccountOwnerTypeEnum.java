377d0a414b28c6c74980fd0768608599a4febd0b
AmilaSamith <amilasamit@gmail.com>
2023-07-13 11:48:17 +0530

    add package change

--- internal-apis/internal-webapps/com.wso2.openbanking.cds.account.type.management.endpoint/src/main/java/com/wso2/openbanking/cds/account/type/management/endpoint/model/SecondaryAccountOwnerTypeEnum.java
+++ internal-apis/internal-webapps/com.wso2.openbanking.cds.account.type.management.endpoint/src/main/java/com/wso2/openbanking/cds/account/type/management/endpoint/model/SecondaryAccountOwnerTypeEnum.java
@@ -7,7 +7,7 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
-package com.wso2.openbanking.cds.consent.extensions.common;
+package com.wso2.openbanking.cds.account.type.management.endpoint.model;
 
 /**
  * ENUM of Secondary Account Owner Types.
