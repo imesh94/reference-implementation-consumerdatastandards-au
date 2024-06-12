2a9563efeb1ed2e6f34eb09851b7399d8bb4e0de
imesh94 <imeshu@wso2.com>
2024-06-12 14:14:18 +0530

    Update metrics unit tests

--- components/com.wso2.openbanking.cds.metrics/src/test/java/com/wso2/openbanking/cds/metrics/data/MockMetricsDataProvider.java
+++ components/com.wso2.openbanking.cds.metrics/src/test/java/com/wso2/openbanking/cds/metrics/data/MockMetricsDataProvider.java
@@ -16,7 +16,12 @@ import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Mock implementation of MetricsDataProvider interface to provide data for unit tests.
@@ -27,18 +32,30 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     public JSONObject getAvailabilityMetricsData() throws OpenBankingException {
 
         String availabilityMetricsData = "{\"records\":" +
-                "[[\"2004\",1714634709,\"scheduled\",1701507600,1701601200]," +
-                "[\"2004\",1714635725,\"scheduled\",1701507600,1701601200]," +
-                "[\"2005\",1714634709,\"incident\",1701774000,1701777600]," +
-                "[\"2005\",1714635725,\"incident\",1701774000,1701777600]," +
-                "[\"2006\",1714634709,\"scheduled\",1702396800,1702400400]," +
-                "[\"2006\",1714635725,\"scheduled\",1702396800,1702400400]," +
-                "[\"2007\",1714634709,\"incident\",1703066400,1703152800]," +
-                "[\"2007\",1714635725,\"incident\",1703066400,1703152800]," +
-                "[\"2028\",1714634709,\"scheduled\",1704002399,1704067199]," +
-                "[\"2028\",1714635725,\"scheduled\",1704002399,1704067199]," +
-                "[\"2029\",1714634709,\"incident\",1704002399,1704067199]," +
-                "[\"2029\",1714635725,\"incident\",1704002399,1704067199]]}";
+                "[[\"2004\"," + getRandomEpochSecondWithinPast13Months() + ",\"scheduled\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2004\"," + getRandomEpochSecondWithinPast13Months() + ",\"scheduled\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2005\"," + getRandomEpochSecondWithinPast13Months() + ",\"incident\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2005\"," + getRandomEpochSecondWithinPast13Months() + ",\"incident\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2006\"," + getRandomEpochSecondWithinPast13Months() + ",\"scheduled\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2006\"," + getRandomEpochSecondWithinPast13Months() + ",\"scheduled\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2007\"," + getRandomEpochSecondWithinPast13Months() + ",\"incident\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2007\"," + getRandomEpochSecondWithinPast13Months() + ",\"incident\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2028\"," + getRandomEpochSecondWithinPast13Months() + ",\"scheduled\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2028\"," + getRandomEpochSecondWithinPast13Months() + ",\"scheduled\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2029\"," + getRandomEpochSecondWithinPast13Months() + ",\"incident\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]," +
+                "[\"2029\"," + getRandomEpochSecondWithinPast13Months() + ",\"incident\"," +
+                getRandomEpochSecondWithinPast13Months() + "," + getRandomEpochSecondWithinPast13Months() + "]]}";
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
             return (JSONObject) parser.parse(availabilityMetricsData);
@@ -51,10 +68,14 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     public JSONObject getInvocationMetricsData() throws OpenBankingException {
 
         String invocationMetricsData = "{\"records\":" +
-                "[[\"HighPriority\",8,1716940800000],[\"LowPriority\",2,1716940800000]," +
-                "[\"Unauthenticated\",1,1716940800000],[\"Unattended\",57,1716940800000]," +
-                "[\"LargePayload\",2,1716940800000],[\"Unattended\",9,1716768000000]," +
-                "[\"HighPriority\",7,1716768000000]]}";
+                "[[\"HighPriority\",8," + getRandomEpochMilliWithinPast7Days() + "],[\"LowPriority\",2," +
+                getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"Unauthenticated\",1," + getRandomEpochMilliWithinPast7Days() + "],[\"Unattended\",57," +
+                getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"LargePayload\",2," + getRandomEpochMilliWithinPast7Days() + "],[\"Unattended\",9," +
+                getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"HighPriority\",7," + getRandomEpochMilliWithinPast7Days() + "]]}";
+
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
             JSONObject jsonObject = (JSONObject) parser.parse(invocationMetricsData);
@@ -78,7 +99,8 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     @Override
     public JSONObject getSessionCountMetricsData() throws OpenBankingException {
 
-        String sessionCountData = "{\"records\":[[2,1716768000000],[2,1716940800000]]}";
+        String sessionCountData = "{\"records\":[[2," + getRandomEpochMilliWithinPast7Days() + "],[2," +
+                getRandomEpochMilliWithinPast7Days() + "]]}";
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
             JSONObject jsonObject = (JSONObject) parser.parse(sessionCountData);
@@ -102,11 +124,11 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     public JSONArray getPeakTPSMetricsData() throws ParseException {
         String peakTPSData = "[{\"event\":{\"total_count\":1,\"MESSAGE_ID\":\"6e80e53c-6a01-4a12-8654-bc5c81141ce9\"," +
                 "\"aspect\":\"authenticated\",\"" +
-                "TIMESTAMP\":1717003786}}," +
+                "TIMESTAMP\":" + getRandomEpochSecondWithinPast7Days() + "}}," +
                 "{\"event\":{\"total_count\":2,\"MESSAGE_ID\":\"6e80e53c-6a01-4a12-8654-bc5c81141ce9\"," +
-                "\"aspect\":\"authenticated\",\"TIMESTAMP\":1716792492}}," +
+                "\"aspect\":\"authenticated\",\"TIMESTAMP\":" + getRandomEpochSecondWithinPast7Days() + "}}," +
                 "{\"event\":{\"total_count\":1,\"MESSAGE_ID\":\"6e80e53c-6a01-4a12-8654-bc5c81141ce9\"," +
-                "\"aspect\":\"authenticated\",\"TIMESTAMP\":1717006916}}]";
+                "\"aspect\":\"authenticated\",\"TIMESTAMP\":" + getRandomEpochSecondWithinPast7Days() + "}}]";
 
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
@@ -119,7 +141,8 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     @Override
     public JSONObject getErrorMetricsData() throws OpenBankingException {
 
-        String errorData = "{\"records\":[[1,1716940800000],[19,1716768000000]]}";
+        String errorData = "{\"records\":[[1," + getRandomEpochMilliWithinPast7Days() + "],[19," +
+                getRandomEpochMilliWithinPast7Days() + "]]}";
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
             JSONObject jsonObject = (JSONObject) parser.parse(errorData);
@@ -142,11 +165,11 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     @Override
     public JSONObject getRejectionMetricsData() throws OpenBankingException {
         String rejectionData = "{\"records\":" +
-                "[[1,1717009253,\"anonymous\"]," +
-                "[1,1717010437,\"anonymous\"]," +
-                "[1,1717006951,\"anonymous\"]," +
-                "[1,1717009249,\"anonymous\"]," +
-                "[1,1717011113,\"anonymous\"]]}";
+                "[[1," + getRandomEpochSecondWithinPast7Days() + ",\"anonymous\"]," +
+                "[1," + getRandomEpochSecondWithinPast7Days() + ",\"anonymous\"]," +
+                "[1," + getRandomEpochSecondWithinPast7Days() + ",\"anonymous\"]," +
+                "[1," + getRandomEpochSecondWithinPast7Days() + ",\"anonymous\"]," +
+                "[1," + getRandomEpochSecondWithinPast7Days() + ",\"anonymous\"]]}";
 
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
@@ -183,13 +206,14 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     @Override
     public JSONObject getTotalResponseTimeMetricsData() throws OpenBankingException {
 
-        String totalResponseTimeData = "{\"records\":[[\"HighPriority\",1.644,1716940800000]," +
-                "[\"LowPriority\",1.485,1716940800000]," +
-                "[\"Unauthenticated\",0.0,1716940800000]," +
-                "[\"Unattended\",267.618,1716940800000]," +
-                "[\"LargePayload\",0.648,1716940800000]," +
-                "[\"Unattended\",8.172,1716768000000]," +
-                "[\"HighPriority\",4.364,1716768000000]]}";
+        String totalResponseTimeData = "{\"records\":[[\"HighPriority\",1.644," +
+                getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"LowPriority\",1.485," + getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"Unauthenticated\",0.0," + getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"Unattended\",267.618," + getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"LargePayload\",0.648," + getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"Unattended\",8.172," + getRandomEpochMilliWithinPast7Days() + "]," +
+                "[\"HighPriority\",4.364," + getRandomEpochMilliWithinPast7Days() + "]]}";
 
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
@@ -202,7 +226,8 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
     @Override
     public JSONObject getSuccessfulInvocationMetricsData() throws OpenBankingException {
 
-        String successfulInvocationsData = "{\"records\":[[68,1716940800000],[68,1716768000000]]}";
+        String successfulInvocationsData = "{\"records\":[[68," + getRandomEpochMilliWithinPast7Days() + "],[68,"
+                + getRandomEpochMilliWithinPast7Days() + "]]}";
         JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
         try {
             JSONObject jsonObject = (JSONObject) parser.parse(successfulInvocationsData);
@@ -220,5 +245,28 @@ public class MockMetricsDataProvider implements MetricsDataProvider {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static long getRandomEpochSecondWithinPast7Days() {
+
+        long now = Instant.now().getEpochSecond();
+        long sevenDaysAgo = Instant.now().minus(7, ChronoUnit.DAYS).getEpochSecond();
+        return ThreadLocalRandom.current().nextLong(sevenDaysAgo, now);
+    }
+
+    private static long getRandomEpochMilliWithinPast7Days() {
+
+        long now = Instant.now().toEpochMilli();
+        long sevenDaysAgo = Instant.now().minusSeconds(7 * 24 * 60 * 60).toEpochMilli();
+        return ThreadLocalRandom.current().nextLong(sevenDaysAgo, now);
+    }
+
+    private static long getRandomEpochSecondWithinPast13Months() {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime thirteenMonthsAgo = now.minus(13, ChronoUnit.MONTHS);
+        Instant thirteenMonthsAgoInstant = thirteenMonthsAgo.atZone(ZoneId.of("GMT")).toInstant();
+        return ThreadLocalRandom.current().nextLong(thirteenMonthsAgoInstant.getEpochSecond(),
+                Instant.now().getEpochSecond());
     }
 }
